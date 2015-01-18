eda7cf0c5b120bb51e297e7cfbddc14f6f6fadf5
Wayne Allen Manselle <viridianforge@gmail.com>
2015-01-18 13:15:23 -0800

    Painful lessons
    I don't think I'll ever forget about CSS comments now.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 /*
 * Main js file to dictate functionality of the main ViridianForge website.
-*
 */
-
-//I should write an onload function that resets the visible page and radio buttons to the about page
 
 //Seed value
 var formerVis = 'about_page';
